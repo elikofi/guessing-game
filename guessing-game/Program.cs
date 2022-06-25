11bffa703016a436bd9d579ed3278f09d46f55b6
elijah amoako <elijahamoako72@gmail.com>
2022-06-25 18:54:36 +0000

    guessing game

---
+++ guessing-game/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+string word = "lion";
+string guess = "";
+
+//to keep track of the number of guesses.
+int count = 0;
+int limit = 3; //limit to the number of times user can guess.
+bool guessesExhausted = false; //guesses exhausted should be false as long as limit hasnt reached.
+
+while (guess != word && !guessesExhausted)
+{
+    if (count < limit)
+    {
+        Console.Write("Guess the name of a wild animal: ");
+        guess = Console.ReadLine();
+        count++;
+    }
+    else
+    {
+        guessesExhausted = true;
+    }
+}
+if (guessesExhausted)
+{
+    Console.WriteLine("You Lose!");
+}
+else
+{
+    Console.WriteLine("You win!");
+}
+
+
+
+
+Console.ReadLine();
+
+
+
+
+
